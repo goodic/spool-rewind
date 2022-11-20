ad41ce67791147e9116a1435b95deaded49d0662
Sergey Gudik <sergey.goodic@gmail.com>
2022-11-20 17:40:49 +0300

    add readme

--- FH_rewind_ver2.ino
+++ FH_rewind_ver2.ino
@@ -7,6 +7,8 @@
 #define motorInterfaceType 1
 #define dcmetr 3305
 #define metr dcmetr * 10
+#define cutterPin 11
+#define cutterSens 12
 
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
@@ -14,6 +16,15 @@ int mspeed = 270;
 
 void runMetr() {
   stepper.move(metr);
+}
+
+void cut() {
+  digitalWrite(cutterPin, HIGH);
+  delay(100); // дать лезвию начать движение и активировать концевик
+  while(digitalRead(cutterSens) == HIGH) {
+    delay(5);
+  }
+  digitalWrite(cutterPin, LOW);
 }
 
 void setup() {
@@ -48,6 +59,11 @@ void loop() {
     }
     if (ch == 'b') {
       stepper.moveTo(0);
+    }
+    if (ch == 'c') {
+      stepper.stop();
+      stepper.disableOutputs();
+      cut();
     }
   }
 }
